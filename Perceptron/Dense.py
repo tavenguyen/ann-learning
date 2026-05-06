9e6919e0de5be0f48b1791ee804b0bc605ce180b
Nguyen Nam <60035507+tavenguyen@users.noreply.github.com>
2026-05-06 13:48:07 +0700

    feat: bo sung activation function cho DenseLayer

--- Perceptron/Dense.py
+++ Perceptron/Dense.py
@@ -1,12 +1,30 @@
 import numpy as np
 
+def linear(x):
+    return x
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return max(x, 0)
+
+def softmax(x):
+    exp_values = np.exp(x - np.max(x, axis=1, keepdims=True))
+    return exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
 class DenseLayer:
     def __init__(self, n_inputs, n_neurons):
         self.weights = np.random.rand(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
     
-    def forward(self, inputs):
+    def forward(self, inputs, activation = None):
         self.output = np.dot(inputs, self.weights) + self.biases
+
+        # activation function
+        if activation is not None:
+            self.output = activation(self.output)
+
         return self.output
 
 
