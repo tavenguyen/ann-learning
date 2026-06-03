fix: tách softmax ra khỏi forward, sử dụng Fushion kết hợp softmax và CCE

Backpropagation/Activation_Softmax_Loss_CategoricalCrossEntropy.py
@@ -9,6 +9,8 @@ class Activation_Softmax_Loss_CategoricalCrossEntropy:
         exp_values = np.exp(inputs - np.max(inputs, axis = 1, keepdims = True))
         self.output = exp_values / np.sum(exp_values, axis = 1, keepdims = True)
 
+
+        samples = len(inputs)
         y_pred = np.clip(inputs, 1e-7, 1 - 1e-7)
 
         # y_pred = [
@@ -19,7 +21,7 @@ class Activation_Softmax_Loss_CategoricalCrossEntropy:
 
         # y_true_1d = [0, 1, 1]
         if(len(y_true.shape) == 1):
-            confidence_score = y_pred[range(inputs), y_true]
+            confidence_score = y_pred[range(samples), y_true]
         # y_true_2d = [
         #     [1, 0, 0],
         #     [0, 1, 0],

Backpropagation/Dense.py
@@ -1,24 +1,23 @@
 import numpy as np
 
 class DenseLayer:
-    def __init__(self, n_neurons, n_inputs, activation_type = 'relu'):
-        self.weights = 0.01 * np.random.randn((n_neurons, n_inputs))
+    def __init__(self, n_neurons, n_inputs, activation_type = 'linear'):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
         self.activation_type = activation_type
 
     def _activation(self, z):
         if self.activation_type == 'relu':
             return np.maximum(z, 0) 
-        elif self.activation_type == 'softmax':
-            exp_values = np.exp(z - np.max(z, axis = 1, keepdims = True))
-            return exp_values / np.sum(exp_values, axis = 1, keepdims = True)
+        return z
         
     def _activation_prime(self, z):
         if self.activation_type == 'relu':
             return np.where(z > 0, 1, 0)
+        return np.ones_like(z) # Đạo hàm linear là 1
 
     def forward(self, inputs):
-        self.dinputs = inputs
+        self.inputs = inputs
 
         # sum
         self.output = np.dot(inputs, self.weights) + self.biases
@@ -27,11 +26,7 @@ class DenseLayer:
         return self.A
     
     def backward(self, dA):
-        if self.activation_type == 'softmax':
-           sum_A_dA = np.sum(self.output * dA, axis = 1, keepdims = True)
-           dZ = self.output * (dA - sum_A_dA)
-        else:
-            dZ = dA * self._activation_prime(self.output)
+        dZ = dA * self._activation_prime(self.output)
         
         self.dW = np.dot(self.inputs.T, dZ)
         self.db = np.sum(dZ, axis = 0, keepdims=True)
@@ -42,18 +37,41 @@ class DenseLayer:
         self.weights -= lr * self.dW
         self.biases -= lr * self.db
 
-class Loss_CategoricalCrossEntropy:
-    def forward(self, inputs, y_true):
-        samples = range(inputs)
+class Loss:
+    def calculate(self, y_pred, y_true):
+        sample_losses = self.forward(y_pred, y_true)
+        loss = np.mean(sample_losses)
+        return loss
 
-        y_pred = np.clip(inputs, 1e-7, 1 - 1e-7)
+class Activation_Softmax_LossCategoricalCrossEntropy(Loss):
+    def forward(self, inputs, y_true):
+        # Tính activation softmax
+        exp_values = np.exp(inputs - np.max(inputs, axis = 1, keepdims = True))
+        self.output =  exp_values / (np.sum(exp_values, axis = 1, keepdims = True))
+
+        # Tính loss
+        samples = len(inputs)
+        y_pred = np.clip(self.output, 1e-7, 1 - 1e-7)
         if len(y_true.shape) == 1:
             confidence_score = y_pred[range(samples), y_true]
         elif len(y_true.shape) == 2:
-            confidence_score = np.sum(y_pred * y_true, axis = 1, keepdims = True)
+            confidence_score = np.sum(y_pred * y_true, axis = 1)
 
         confidence_score = -np.log(confidence_score)
         return confidence_score
+    
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+
+        self.dinputs = dvalues.copy()
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis = 1)
+
+        # dZ = A - Y
+        self.dinputs[range(samples), y_true] -= 1 
+        self.dinputs /= samples
+        return self.dinputs
+    
 
 class Spiral:
     def __init__(self, n_points, n_classes, n_dimensions):
@@ -79,14 +97,37 @@ K = 3
 D = 2
 inputs = Spiral(n_points = N, n_classes = K, n_dimensions = D)
 
-layer1 = DenseLayer(n_inputs = 2, n_neurons = 2)
-layer2 = DenseLayer(n_inputs = 2, n_neurons = 3, activation_type = 'relu')
-layer3 = DenseLayer(n_inputs = 3, n_neurons = 3, activation_type = 'softmax')
+layer1 = DenseLayer(n_inputs = 2, n_neurons = 3, activation_type = 'relu')
+layer2 = DenseLayer(n_inputs = 3, n_neurons = 3)
+loss_activation = Activation_Softmax_LossCategoricalCrossEntropy()
 
+
+# Forward pass
 layer1.forward(inputs.P)
 layer2.forward(layer1.A)
-layer3.forward(layer2.A)
 
-losses = Loss_CategoricalCrossEntropy()
-loss = losses.forward(layer3.A, inputs.C)
-print("Losses: %f", loss)
+# Loss calculate
+loss = loss_activation.calculate(layer2.A, inputs.L)
+print("Losses: ", loss)
+
+# Accuracy calculate
+predictions = np.argmax(loss_activation.output, axis=1)
+accuracy = np.mean(predictions == inputs.L)
+
+# --- BACKWARD PASS ---
+# Truyền Softmax Output và Nhãn vào để lấy dZ đầu tiên
+loss_activation.backward(loss_activation.output, inputs.L)
+
+# Lớp 2 nhận dZ từ lớp Fusion (loss_activation.dinputs), trả ra dX_2
+dX_2 = layer2.backward(loss_activation.dinputs)
+
+# Lớp 1 nhận dX_2 (coi như dA của nó), trả ra dX_1
+dX_1 = layer1.backward(dX_2)
+
+# Print gradients of weights and biaises
+print("Layer1: dweights, dbiases")
+print(layer1.dW)
+print(layer1.db)
+print("Layer2: dweights, dbiases")
+print(layer2.dW)
+print(layer2.db)