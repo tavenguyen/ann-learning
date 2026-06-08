e93652e7fbfaea2884f8f5dfe966ec631d1a3d7e
Nguyen Nam <60035507+tavenguyen@users.noreply.github.com>
2026-06-08 10:39:13 +0700

    feat: xây dựng class Optimizer_Adam

--- Optimization/Optimizer.py
+++ Optimization/Optimizer.py
@@ -124,3 +124,79 @@ class Optimizer_RMSProp:
 
     def post_update_params(self):
         self.iterations += 1
+
+class Optimizer_Adam:
+    def __init__(self, lr = 1.0, decay_rate = 0.0, beta_1 = 0.9, beat_2 = 0.999, epsilon = 1e-7):
+        self.initial_lr = lr
+        self.current_lr = lr
+        self.decay_rate = decay_rate
+        self.beta_1 = beta_1
+        self.beta_2 = self.beta_2
+        self.epsilon = epsilon
+        self.iterations = 0
+
+    def pre_update_params(self):
+        self.current_lr = self.current_lr
+
+    def update_params(self, layer):
+        if not hasattr(layer, "weight_momentums"):
+            layer.weight_momentums = np.zeros_like(layer.weights)
+            layer.bias_momentums = np.zeros_like(layer.biases)
+
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+        layer.weight_momentums = (
+            self.beta_1 * layer.weight_momentums
+            + (1 - self.beta_1) * layer.dW
+        )
+
+        layer.bias_momentums = (
+            self.beta_1 * layer.bias_momentums
+            + (1 - self.beta_1) * layer.dB
+        )
+
+        layer.weight_cache = (
+            self.beta_2 * layer.weight_cache
+            + (1 - self.beta_2) * layer.dW ** 2
+        )
+
+        layer.bias_cache = (
+            self.beta_2 * layer.bias_cache
+            + (1 - self.beta_2) * layer.dB ** 2
+        )
+
+        weight_momentums_corrected = (
+            layer.weight_momentums
+            / (1 - self.beta_1 ** (self.iterations + 1))
+        )
+
+        bias_momentums_corrected = (
+            layer.bias_momentums
+            / (1 - self.beta_1 ** (self.iterations + 1))
+        )
+
+        weight_cache_corrected = (
+            layer.weight_cache
+            / (1 - self.beta_2 ** (self.iterations + 1))
+        )
+
+        bias_cache_corrected = (
+            layer.bias_cache
+            / (1 - self.beta_2 ** (self.iterations + 1))
+        )
+
+        layer.weights -= (
+            self.current_lr
+            * weight_momentums_corrected
+            / (np.sqrt(weight_cache_corrected) + self.epsilon)
+        )
+
+        layer.biases -= (
+            self.current_lr
+            * bias_momentums_corrected
+            / (np.sqrt(bias_cache_corrected) + self.epsilon)
+        )
+
+    def post_update_params(self):
+        self.iterations += 1
