b0b4939bd9a53039ab34cc2541e816a5743c6656
Nguyen Nam <60035507+tavenguyen@users.noreply.github.com>
2026-06-23 09:52:49 +0700

    feat: cập nhật hàm forward, backward trong DenseLayer

--- Model/model.py
+++ Model/model.py
@@ -4,6 +4,16 @@ class DenseLayer:
     def __init__(self, n_neurons, n_inputs):
         self.weights = np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+    
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis = 0, keepdims=True)
+        self.dinputs = np.dot(dvalues, self.weights.T)
+        return self.dinputs
 
 class Activation_Linear:
     def forward(self, inputs):
